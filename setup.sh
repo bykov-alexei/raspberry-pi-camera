a26bffb690a0bc4624b459155b45c35fa592b903
Alexei <mail@bykov-alexei.ru>
2022-02-05 16:27:48 +0800

    Fixed setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 sudo raspi-config
 
+sudo cp rtsp.service /lib/systemd/system/
+sudo chmod 644 /lib/systemd/system/rtsp.service
+sudo systemctl daemon-reload
+sudo systemctl enable rtsp.service
+# sudo reboot
