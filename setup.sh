f91a68f978f1a3c6c92d233ae97ea5152b28a8c9
Alexei <mail@bykov-alexei.ru>
2022-02-05 15:30:42 +0800

    updated seteup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-raspbi-config
+raspi-config
 update-rc.d ssh defaults
 service ssh start
 
 apt-get install cmake liblog4cpp5-dev libv4l-dev git -y
-git clone https://github.com/mpromonet/v4l2rtspserver.git
+git clone https://github.com/mpromonet/v4l2rtspserver.git ; cd v4l2rtspserver/ ; cmake . ; make ; sudo make install
