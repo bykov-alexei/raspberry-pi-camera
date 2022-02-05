49538102af4ca37a3b256b394ac69078e8d0521c
Alexei <mail@bykov-alexei.ru>
2022-02-05 16:31:05 +0800

    Add reboot to setup

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ sudo cp rtsp.service /lib/systemd/system/
 sudo chmod 644 /lib/systemd/system/rtsp.service
 sudo systemctl daemon-reload
 sudo systemctl enable rtsp.service
-# sudo reboot
+sudo reboot
