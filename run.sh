e3a190accb2e067b5c0f172ff1dd896508c66e64
Alexei <mail@bykov-alexei.ru>
2022-02-17 20:20:05 +0800

    Switched to tcp

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-raspivid -o - -t 0 -rot 180 -w 1920 -h 1080 -fps 30 -b 2000000 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream}' :demux=h264
+raspivid -o - -t 0 -rot 180 -w 800 -h 600 -fps 30 -b 2000000 | nc -k -l 8554
